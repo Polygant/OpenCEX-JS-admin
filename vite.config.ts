529f6c931a5679384bac0a222e5539c363931cf6
botjoker <botjoker@gmail.com>
2023-04-21 12:26:10 +0300

    Fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 export default defineConfig({
   plugins: [vue()],
   publicDir: path.resolve(__dirname, "./src/static"),
+  base: "/newadmin",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
