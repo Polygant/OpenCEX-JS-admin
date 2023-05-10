0d2e0e5ad17d96c8281cf23f3f650fb191566999
botjoker <botjoker@gmail.com>
2023-05-10 16:55:13 +0300

    Fix path

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 // @ts-ignore
 import path from "path";
+import localConfig from './src/local_config';
 export default defineConfig({
   plugins: [vue()],
   publicDir: path.resolve(__dirname, "./src/static"),
-  base: "/newadmin",
+  base: `/${localConfig.base}`,
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
